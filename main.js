7e542296c87beb56b940acde60a005df0f391578
Kyle Dumovic <kdumovic@gmail.com>
2021-02-20 14:11:47 -0800

    Import css and js into index.html. Refactor js with globals. Adjust canvas for retina displays.

--- main.js
+++ main.js
@@ -1,47 +1,53 @@
-var rectStrokeWidth = 1;
-var rectWidth = 25;
-var rectHeight = 25;
-var rectStrokeColor = '#000';
+var globals = {};
 
-var gapBetweenRects = 50;
+function main() {
 
-var htmlCanvas = document.querySelector("canvas");
-var ctx = htmlCanvas.getContext("2d");
+  globals.rectStrokeWidth = 1;
+  globals.rectWidth = 25;
+  globals.rectHeight = 25;
+  globals.rectStrokeColor = '#000';
 
-var currentMouseX = 0;
-var currentMouseY = 0;
-var offsetMultipler = 0.05;
-var maxOffset = gapBetweenRects * 0.5;
+  globals.gapBetweenRects = 50;
 
-initialize();
+  globals.htmlCanvas = document.querySelector("canvas");
+  globals.ctx = globals.htmlCanvas.getContext("2d");
+
+  globals.currentMouseX = 0;
+  globals.currentMouseY = 0;
+  globals.offsetMultipler = 0.05;
+  globals.maxOffset = globals.gapBetweenRects * 0.5;
+
+  initialize();
+  toggleDarkMode();
+}
 
 function initialize() {
 
   window.addEventListener('resize', resizeCanvas, false);
 
   ['touchstart','touchmove','touchend','mousemove'].forEach(function(e) {
-    window.addEventListener(e, movementHandler.bind(this), false);
+    window.addEventListener(e, movementHandler);
   });
 
   function movementHandler(e, currentMouseX, currentMouseY) {
     if (e.type == 'touchstart' || e.type == 'touchmove' || e.type == 'touchend') {
         var evt = (typeof e.originalEvent === 'undefined') ? e : e.originalEvent;
         var touch = evt.touches[0] || evt.changedTouches[0];
-        this.currentMouseX = touch.pageX;
-        this.currentMouseY = touch.pageY;
+        globals.currentMouseX = touch.pageX;
+        globals.currentMouseY = touch.pageY;
         redraw();
     } else if (e.type == 'mousemove') {
         // currentMouseX_perc = evt.clientX / innerWidth;
         // currentMouseY_perc = evt.clientY / innerHeight;
-        this.currentMouseX = e.clientX;
-        this.currentMouseY = e.clientY;
+        globals.currentMouseX = e.clientX;
+        globals.currentMouseY = e.clientY;
         redraw();
     }
   }
 
   // set default to center
-  currentMouseX = window.innerWidth/2;
-  currentMouseY = window.innerHeight/2;
+  globals.currentMouseX = window.innerWidth/2;
+  globals.currentMouseY = window.innerHeight/2;
 
   resizeCanvas();
 }
@@ -49,47 +55,51 @@ function initialize() {
 function redraw() {
 
   // clear canvas
-  ctx.clearRect(0, 0, htmlCanvas.width, htmlCanvas.height);
+  globals.ctx.clearRect(0, 0, globals.htmlCanvas.width, globals.htmlCanvas.height);
 
   // center coordinates
   let cx = window.innerWidth/2;
   let cy = window.innerHeight/2;
 
   // draw a rectangle in the exact center
-  drawRectangleFromCenter(cx, cy, rectWidth, rectHeight, 'blue', 1);
+  drawRectangleFromCenter(cx, cy, globals.rectWidth, globals.rectHeight, 'blue', 1);
 
   // draw mouse rectangle
   // drawRectangle(currentMouseX - rectWidth/2, currentMouseY - rectHeight/2, rectWidth, rectHeight);
 
   // the math is more complicated here because we always want one rectangle in the exact center; we aren't simply trying to see how many would fit
-  let numCols = Math.floor((cx-(rectWidth/2))/(gapBetweenRects+rectWidth))*2+1;
-  let numRows = Math.floor((cy-(rectHeight/2))/(gapBetweenRects+rectHeight))*2+1;
+  let numCols = Math.floor((cx-(globals.rectWidth/2))/(globals.gapBetweenRects+globals.rectWidth))*2+1;
+  let numRows = Math.floor((cy-(globals.rectHeight/2))/(globals.gapBetweenRects+globals.rectHeight))*2+1;
 
-  let initialOffsetFromLeft = (window.innerWidth - numCols*rectWidth - (numCols-1)*gapBetweenRects)/2;
-  let initialOffsetFromTop = (window.innerHeight - numRows*rectHeight - (numRows-1)*gapBetweenRects)/2;
+  let initialOffsetFromLeft = (window.innerWidth - numCols*globals.rectWidth - (numCols-1)*globals.gapBetweenRects)/2;
+  let initialOffsetFromTop = (window.innerHeight - numRows*globals.rectHeight - (numRows-1)*globals.gapBetweenRects)/2;
 
   // draw rows of rectangles from top left
   for (let rowNum = 0; rowNum < numRows; rowNum++) {
     for (let colNum = 0; colNum < numCols; colNum++) {
-      let x1 = initialOffsetFromLeft + (colNum * (rectWidth + gapBetweenRects));
-      let y1 = initialOffsetFromTop + (rowNum * (rectHeight + gapBetweenRects));
+      let x1 = initialOffsetFromLeft + (colNum * (globals.rectWidth + globals.gapBetweenRects));
+      let y1 = initialOffsetFromTop + (rowNum * (globals.rectHeight + globals.gapBetweenRects));
       drawRectanglePair(x1, y1);
     }
   }
 }
 
 function resizeCanvas() {
-  htmlCanvas.width = window.innerWidth;
-  htmlCanvas.height = window.innerHeight;
+  globals.htmlCanvas.width = window.innerWidth*window.devicePixelRatio;
+  globals.htmlCanvas.height = window.innerHeight*window.devicePixelRatio;
+  globals.htmlCanvas.style.width = `${window.innerWidth}px`;
+  globals.htmlCanvas.style.height = `${window.innerHeight}px`;
+  globals.ctx.scale(window.devicePixelRatio,window.devicePixelRatio); // adjust for retina displays
+
   redraw();
 }
 
 function drawRectangle(x1, y1, width, height, strokeColor, strokeWidth) {
-  ctx.beginPath();
-  ctx.rect(x1, y1, width, height);
-  ctx.strokeStyle = strokeColor;
-  ctx.lineWidth = strokeWidth;
-  ctx.stroke();
+  globals.ctx.beginPath();
+  globals.ctx.rect(x1, y1, width, height);
+  globals.ctx.strokeStyle = strokeColor;
+  globals.ctx.lineWidth = strokeWidth;
+  globals.ctx.stroke();
 }
 
 function drawRectangleFromCenter(cx, cy, width, height, strokeColor, strokeWidth) {
@@ -104,23 +114,23 @@ function drawRectanglePair(f_x1, f_y1) {
   // calculate the offset as a function of current mouse placement by getting the distance between the fixed rectangle and the mouse position
 
   // dynamic offset
-  let xOffset = (currentMouseX - rectWidth/2 - f_x1) * offsetMultipler,
-      yOffset = (currentMouseY - rectHeight/2 - f_y1) * offsetMultipler;
+  let xOffset = (globals.currentMouseX - globals.rectWidth/2 - f_x1) * globals.offsetMultipler,
+      yOffset = (globals.currentMouseY - globals.rectHeight/2 - f_y1) * globals.offsetMultipler;
 
   // apply caps to offsets
-  xOffset = (Math.abs(xOffset) > maxOffset) ? (Math.sign(xOffset) * maxOffset) : xOffset;
-  yOffset = (Math.abs(yOffset) > maxOffset) ? (Math.sign(yOffset) * maxOffset) : yOffset;
+  xOffset = (Math.abs(xOffset) > globals.maxOffset) ? (Math.sign(xOffset) * globals.maxOffset) : xOffset;
+  yOffset = (Math.abs(yOffset) > globals.maxOffset) ? (Math.sign(yOffset) * globals.maxOffset) : yOffset;
 
   // draw fixed rectangle
-  drawRectangle(f_x1, f_y1, rectWidth, rectHeight, rectStrokeColor, rectStrokeWidth);
+  drawRectangle(f_x1, f_y1, globals.rectWidth, globals.rectHeight, globals.rectStrokeColor, globals.rectStrokeWidth);
 
   // draw offset rectangle
-  drawRectangle(f_x1 + xOffset, f_y1 + yOffset, rectWidth, rectHeight, rectStrokeColor, rectStrokeWidth);
+  drawRectangle(f_x1 + xOffset, f_y1 + yOffset, globals.rectWidth, globals.rectHeight, globals.rectStrokeColor, globals.rectStrokeWidth);
 
   let f_corners = [[f_x1, f_y1],
-                   [f_x1 + rectWidth, f_y1],
-                   [f_x1 + rectWidth, f_y1 + rectHeight],
-                   [f_x1, f_y1 + rectHeight]];
+                   [f_x1 + globals.rectWidth, f_y1],
+                   [f_x1 + globals.rectWidth, f_y1 + globals.rectHeight],
+                   [f_x1, f_y1 + globals.rectHeight]];
 
   // connect the corners of each rectange
   f_corners.forEach(drawLineToOffsetCorner);
@@ -128,23 +138,25 @@ function drawRectanglePair(f_x1, f_y1) {
   function drawLineToOffsetCorner(cornerPair) {
     let startx = cornerPair[0],
         starty = cornerPair[1];
-    ctx.beginPath();
-    ctx.moveTo(startx, starty);
-    ctx.lineTo(startx + xOffset, starty + yOffset);
-    ctx.stroke();
+    globals.ctx.beginPath();
+    globals.ctx.moveTo(startx, starty);
+    globals.ctx.lineTo(startx + xOffset, starty + yOffset);
+    globals.ctx.stroke();
   }
 
 }
 
 function toggleDarkMode() {
-  if (rectStrokeColor == '#000') {
-    rectStrokeColor = '#fff';
-    htmlCanvas.style['background-color'] = '#000';
+  if (globals.rectStrokeColor == '#000') {
+    globals.rectStrokeColor = '#fff';
+    globals.htmlCanvas.style['background-color'] = '#000';
     document.getElementById('dark-mode-btn').innerHTML = 'Light mode!';
   } else {
-    rectStrokeColor = '#000';
-    htmlCanvas.style['background-color'] = '#fff';
+    globals.rectStrokeColor = '#000';
+    globals.htmlCanvas.style['background-color'] = '#fff';
     document.getElementById('dark-mode-btn').innerHTML = 'Dark mode!';
   }
   redraw();
 }
+
+window.onload = main();
